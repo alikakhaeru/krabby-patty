df6a195c28e8e1653581bbc8ad9629f2c668354d
unknown <alikabimo26@gmail.com>
2025-09-16 09:42:33 +0700

    inisiasi resep krabby patty

---
+++ Resep.java
@@ -0,0 +1,8 @@
+public class Resep {
+    public static void main(String[] args) {
+        System.out.println("Resep Krabby Patty");
+        System.out.println("--------------------");
+        System.out.println("1. Bawang Merah");
+        System.out.println("2. Mentimun");
+    }
+}
