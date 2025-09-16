201eef3884a724934e536978f8a95a6f3282e76e
Nama Anda <faizalba74@gmail.com>
2025-09-16 10:02:22 +0700

    insisiasi Resep Ke-2

--- Resep.java
+++ Resep.java
@@ -4,5 +4,11 @@ public class Resep {
         System.out.println("--------------------");
         System.out.println("1. Bawang Merah");
         System.out.println("2. Mentimun");
+        System.out.println("3. Tomat");
+        System.out.println("4. Selada");
+        System.out.println("5. Roti");
+        System.out.println("6. Patty");
+        System.out.println("7. Keju");
+        System.out.println("8. Saus Rahasia");
     }
 }
